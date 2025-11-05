ebcd6eab9f2e82bf68c48d6903ef883cc4431ddd
Tianhao Wu <wth569749099@gmail.com>
2025-11-04 17:14:43 -0800

    fix sdk

--- sdk/chat/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Chat provider clients exposed by the RLLM SDK."""
-
-from .openai_client import OpenAIChatClient
-from .simple_chat_client import SimpleTrackedChatClient
-
-__all__ = ["OpenAIChatClient", "SimpleTrackedChatClient"]
