4cd5310f9b71ae9def16313386d57c191b4902c0
Tianhao Wu <wth569749099@gmail.com>
2025-11-18 18:23:10 -0800

    revert

--- examples/sdk/modal_image.py
+++ examples/sdk/modal_image.py
@@ -2,7 +2,7 @@ import os
 
 import modal
 
-image = modal.Image.from_registry("verlai/verl:app-verl0.5-vllm0.10.0-mcore0.13.0-te2.2").run_commands("git clone --recurse-submodules https://github.com/thwu1/rllm.git && cd rllm && git checkout 5ca63986561b98e811475dc3dabdcad3772141b2 && cd verl && git checkout 60d8a62 && cd .. ").run_commands("pip install -e verl/").run_commands("pip install -e .").run_commands("pip install litellm[proxy] asgiref==3.10.0 aiosqlite")
+image = modal.Image.from_registry("verlai/verl:app-verl0.5-vllm0.10.0-mcore0.13.0-te2.2").run_commands("""git clone --recurse-submodules https://github.com/thwu1/rllm.git && cd rllm && git checkout 5ca63986561b98e811475dc3dabdcad3772141b2 && cd verl && git checkout 60d8a62 && cd .. && pip install -e verl/ && pip install -e . && pip install litellm[proxy] asgiref==3.10.0 aiosqlite""")
 app = modal.App("rllm-v1")
 
 
