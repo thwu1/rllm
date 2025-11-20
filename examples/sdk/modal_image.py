b87ca605aa858a34568ba1f948375079ce6fcf82
Tianhao Wu <wth569749099@gmail.com>
2025-11-19 16:36:00 -0800

    Squashed commit of the following:
    commit 2104e5eeb94b4400ef57b4dcf33d00450095453e
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Wed Nov 19 16:30:54 2025 -0800
    
        update nb & commit
    
    commit f7e0d1baee2d14be7c907c5a62f03797bd78e13a
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Tue Nov 18 20:02:42 2025 -0800
    
        clear output
    
    commit d7ee81ca81f92b1a8d61908c89e8d29ee50f70ff
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Tue Nov 18 20:01:16 2025 -0800
    
        better tutorial
    
    commit dda7d8db216556050a318728dc75780a72c0402c
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Tue Nov 18 19:03:26 2025 -0800
    
        update tutorial
    
    commit 4cd5310f9b71ae9def16313386d57c191b4902c0
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Tue Nov 18 18:23:10 2025 -0800
    
        revert
    
    commit 72a3f4703f67849f3fa56c81b1fe00c1f08790b9
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Tue Nov 18 18:08:35 2025 -0800
    
        update commit
    
    commit 5ca63986561b98e811475dc3dabdcad3772141b2
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Tue Nov 18 18:06:31 2025 -0800
    
        add modal image
    
    commit ab683bd0decd0dc18050a3cf5a83873a671f5db4
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Tue Nov 18 15:50:08 2025 -0800
    
        rm wandb
    
    commit ad8efdfe14e0b1a086e77e71606283b50897ffad
    Author: Tianhao Wu <wth569749099@gmail.com>
    Date:   Tue Nov 18 15:02:04 2025 -0800
    
        one gpu, fix ray env

---
+++ examples/sdk/modal_image.py
@@ -0,0 +1,11 @@
+import os
+
+import modal
+
+image = modal.Image.from_registry("verlai/verl:app-verl0.5-vllm0.10.0-mcore0.13.0-te2.2").run_commands("""git clone --recurse-submodules https://github.com/thwu1/rllm.git && cd rllm && git checkout 4cd5310 && cd verl && git checkout 60d8a62 && cd .. && pip install -e verl/ && pip install -e . && pip install litellm[proxy] asgiref==3.10.0 aiosqlite""")
+app = modal.App("rllm-v1")
+
+
+@app.function(image=image)
+def tutorial() -> list[dict]:
+    os.system("pip list")
